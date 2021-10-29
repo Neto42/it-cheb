aeaf95d19f56ded8966a392d2e1d0ae4bceb5c6d
kirishito <93390354+kirishito@users.noreply.github.com>
2021-10-29 19:09:37 +0300

    изменила настройки бд

--- backend/it-cheb/settings.py
+++ backend/it-cheb/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'it-cheb1',
         'USER': 'postgres',
-        'PASSWORD': '12345678',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
