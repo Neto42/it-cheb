be238cc4c8bea6788cdf399fc0551027ed25e7d2
kirishito <93390354+kirishito@users.noreply.github.com>
2021-10-30 12:52:26 +0300

    таблица города

---
+++ backend/city/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class City(models.Model):
+    city = models.CharField(max_length=25)
+    district = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    house = models.CharField(max_length=10)
